f09633949fc9fe18721ac69b7fc16d36bc24b14f
Julio102030 <juliocasa01@gmail.com>
2026-03-20 13:54:29 -0500

    Add files via upload

---
+++ VisioraApp/sw.js
@@ -0,0 +1,22 @@
+{
+  "name": "Visiora AI",
+  "short_name": "Visiora",
+  "description": "Asistente inteligente de visión, lectura y navegación.",
+  "start_url": "./index.html",
+  "display": "fullscreen",
+  "background_color": "#000000",
+  "theme_color": "#000000",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "LOGO VISIORA AI.jpg",
+      "sizes": "192x192",
+      "type": "image/jpeg"
+    },
+    {
+      "src": "LOGO VISIORA AI.jpg",
+      "sizes": "512x512",
+      "type": "image/jpeg"
+    }
+  ]
+}
